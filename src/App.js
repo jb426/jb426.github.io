85f07fa149e94e0f1c9f55f6f4d7898f91d08d47
Joey B <jbrucie426@gmail.com>
2025-12-19 10:36:34 -0600

    start of nav

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 import React from 'react';
 import './App.css';
-import Beams from './Beams';
+// import Beams from './Beams';
 
 const App = () => {
   return (
+    // <div className="mainContainer">
+    //   <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
+    //     <Beams
+    //       beamWidth={2}
+    //       beamHeight={30}
+    //       beamNumber={20}
+    //       lightColor="#ffffff"
+    //       speed={2}
+    //       noiseIntensity={1.75}
+    //       scale={0.2}
+    //       rotation={30}
+    //     />
+    //   </div>
+    //   <section className="headNav">
+    //     <h1>Hi I'm Joey</h1>
+    //   </section>
+    //   <section id="about"></section>
+    //   <section id="experience"></section>
+    //   <section id="projects"></section>
+    //   <section id="footer"></section>
+    // </div>
+
     <div className="mainContainer">
-      <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
-        <Beams
-          beamWidth={2}
-          beamHeight={30}
-          beamNumber={20}
-          lightColor="#ffffff"
-          speed={2}
-          noiseIntensity={1.75}
-          scale={0.2}
-          rotation={30}
-        />
-      </div>
       <section className="headNav">
-        <h1>Hi I'm Joey</h1>
+        <div id="title">
+          <h2>Joseph Brucie</h2>
+          <h3>Front End Developer</h3>
+        </div>
+        <div id="nav">
+          <button class="navBtn">Resume</button>
+          <button class="navBtn">LinkedIn</button>
+          <button class="navBtn">GitHub</button>
+        </div>
       </section>
-      <section id="about"></section>
-      <section id="experience"></section>
-      <section id="projects"></section>
-      <section id="footer"></section>
     </div>
   );
 }
