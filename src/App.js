51ede5e5b50386cbcde6fb33045f82965cd7232b
Joey B <jbrucie426@gmail.com>
2026-01-14 15:55:21 -0600

    Nav bar implementation

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
 import React from 'react';
 import './App.css';
-// import Beams from './Beams';
 
 const App = () => {
   return (
-    // <div className="mainContainer">
-    //   <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
-    //     <Beams
-    //       beamWidth={2}
-    //       beamHeight={30}
-    //       beamNumber={20}
-    //       lightColor="#ffffff"
-    //       speed={2}
-    //       noiseIntensity={1.75}
-    //       scale={0.2}
-    //       rotation={30}
-    //     />
-    //   </div>
-    //   <section className="headNav">
-    //     <h1>Hi I'm Joey</h1>
-    //   </section>
-    //   <section id="about"></section>
-    //   <section id="experience"></section>
-    //   <section id="projects"></section>
-    //   <section id="footer"></section>
-    // </div>
-
     <div className="mainContainer">
       <section className="headNav">
         <div id="title">
@@ -33,8 +10,8 @@ const App = () => {
           <h3>Front End Developer</h3>
         </div>
         <div id="nav">
-          <button class="navBtn">Resume</button>
-          <button class="navBtn">LinkedIn</button>
+          <a href="/brucie_resume_2026.pdf" target="_blank" class="mainNavBtn">Resume</a>
+          <a href="https://www.linkedin.com/in/joebrucie/" target="_blank" class="navBtn">LinkedIn</a>
           <button class="navBtn">GitHub</button>
         </div>
       </section>
