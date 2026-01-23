55e11a0c57fb1b7ac9dba92c95df85487a5535bf
Joey B <jbrucie426@gmail.com>
2026-01-23 16:03:17 -0600

    First additions to experience section

--- src/App.js
+++ src/App.js
@@ -30,10 +30,14 @@ const App = () => {
       <section className="intro">
         <h1>Hi, I'm Joey</h1>
         <div className="introBio">
-          <img src="profile_pic.jpg" alt="Profile Picture"></img>
+          <img
+            src="profile_pic.jpg"
+            className="profilePic"
+            alt="Profile Picture"
+          ></img>
           <p>
             I'm a passionate front-end engineer with a strong interest in UI/UX,
-            web, and interactive design & development. With over three years of
+            web, and interactive design & development. With over four years of
             professional experience, I've specialized in building intuitive,
             human-centered, and accessible digital products.
             <br />
@@ -51,6 +55,82 @@ const App = () => {
             <br />
             <br /> I am pursuing my career goals of working in UI/UX design &
             development and I'm open to relocation opportunities.
+          </p>
+        </div>
+      </section>
+      <section className="experience">
+        <h1>Experience</h1>
+        <div className="experienceContent">
+          <h4>Jul 2025 - Present</h4>
+          <div className="experienceTitle">
+            <img
+              src="marquette_logo.png"
+              className="experienceLogo"
+              alt="Marquette Logo"
+            ></img>
+            <h5>
+              Marquette University | <em>Web Application Specialist</em>
+            </h5>
+          </div>
+          <p>
+            Web and digital platform specialist managing the Law School's Drupal
+            and WordPress sites and related applications. Oversee performance
+            and user access while designing and coding responsive email
+            campaigns, event materials, and promotional banners. Troubleshoot
+            complex web issues and collaborate with faculty, staff, and vendors
+            to enhance user experience, integrate systems, and implement process
+            improvements that advance the school's digital strategy.
+          </p>
+        </div>
+        <div className="experienceContent">
+          <h4>Sept 2024 - Apr 2025</h4>
+          <div className="experienceTitle">
+            <img
+              src="ag_logo.png"
+              className="experienceLogo"
+              alt="American Greetings Logo"
+            ></img>
+            <h5>
+              American Greetings | <em>Software Engineer 1</em>
+            </h5>
+          </div>
+          <p>
+            Designed and developed premium interactive E-Card products using
+            modern front-end technologies (Vue.js, JavaScript, Pixi.js,
+            HTML/CSS, Adobe Creative Suite, Figma), while streamlining
+            workflows, improving accessibility, mentoring interns, and
+            collaborating across QA and product teams to enhance performance and
+            user engagement.
+          </p>
+        </div>
+        <div className="experienceContent">
+          <h4>Sept 2022 - Apr 2024</h4>
+          <h5>
+            American Greetings | <em>Associate Software Engineer</em>
+          </h5>
+          <p>
+            Supported the design and development of interactive E-Card products
+            through asset updates, animation adjustments, and code optimization
+            to improve performance. Managed Bitbucket repositories, developed
+            templates to streamline future projects, and collaborated with QA
+            teams to identify and resolve product defects. Led project with
+            intern to create a premium E-Card using Pixi.js, GSAP, Adobe
+            Animate, and CSS, while assisting in the intern hiring process and
+            providing ongoing mentorship throughout development.
+          </p>
+        </div>
+        <div className="experienceContent">
+          <h4>Jan 2022 - Sept 2022</h4>
+          <h5>
+            American Greetings | <em>Interactive Developer Intern</em>
+          </h5>
+          <p>
+            Developed interactive E-Card products and web applications using
+            Figma, JavaScript, Pixi.js, HTML, and CSS to deliver engaging
+            digital experiences. Improved accessibility for people with
+            disabilities by restructuring code across 100+ E-Cards as part of a
+            large-scale initiative focused on meeting modern web standards and
+            enhancing usability.
           </p>
         </div>
       </section>
